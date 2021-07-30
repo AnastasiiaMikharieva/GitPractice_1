aa6a4f67f636e154a2a77f3062507c27dcde1e47
AnastasiiaMikharieva <85961200+AnastasiiaMikharieva@users.noreply.github.com>
2021-07-30 10:50:04 -0400

    mastre collection work is done

--- src/day4/Conflict.java
+++ src/day4/Conflict.java
@@ -1,11 +1,12 @@
 package day4;
 
 public class Conflict {
-<<<<<<< HEAD
+
     //this is master branch content
-=======
+
         //I can type anything here
         //and it will become final merge result
     //This is collection branch content
->>>>>>> collection
+    //This is a chanche
+
 }
